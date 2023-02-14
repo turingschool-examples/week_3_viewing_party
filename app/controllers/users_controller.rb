25fb547b83b8f044cd087bfc1afebe2a49e5d4af
Alex Pitzel <pitzelalex@gmail.com>
2023-02-14 13:16:22 -0500

    feat: complete tuesday challenge part 1 task 1 and 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,43 @@
-class UsersController <ApplicationController 
-    def new 
-        @user = User.new()
-    end 
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
-    def show 
-        @user = User.find(params[:id])
-    end 
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def create 
-        user = User.create(user_params)
-        if user.save
-            redirect_to user_path(user)
-        else  
-            flash[:error] = user.errors.full_messages.to_sentence
-            redirect_to register_path
-        end 
-    end 
+  def create
+    user = User.create(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      flash[:error] = user.errors.full_messages.to_sentence
+      redirect_to register_path
+    end
+  end
 
-    def login_form
+  def login_form; end
 
-    end 
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/users/#{session[:user_id]}"
+    else
+      flash[:error] = 'Bad Credentials, try again.'
+      redirect_to '/login'
+    end
+  end
 
-    def login_user
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
-            redirect_to "/users/#{user.id}"
-        else 
-            flash[:error] = "Bad Credentials, try again."
-            redirect_to "/login" 
-        end 
-    end 
+  def logout_user
+    session.delete('user_id')
+    redirect_to root_path
+  end
 
-    private 
+  private
 
-    def user_params 
-        params.require(:user).permit(:name, :email, :password)
-    end 
-end 
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
